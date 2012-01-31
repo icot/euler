d1e776ee609fb20f28c508df2a819f9142428db3
icot <icot@users.noreply.github.com>
2012-01-31 18:56:11 +0100

    Added factorize, sieve_u32, sieve_u64 and isprime

--- primegen.c
+++ primegen.c
@@ -1,21 +1,115 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <malloc.h>
+#include <stdbool.h>
+#include <math.h>
 
-long* sieve(long N){
-    long *result = (long *) malloc(((N/2)+1) * sizeof(long));
-    result[0] = 2;
-    for (long pos = 1; pos < (N/2) +1 ; pos++){
-        result[pos] = (pos-1)*2 + 3;
+uint64_t* sieve_u64(uint64_t N){
+    printf("Allocating memory for candidates: %llu bytes\n", ((N+1)/2 * sizeof(uint64_t)));
+    uint64_t *candidates = (uint64_t *) calloc(((N+1)/2), sizeof(uint64_t));
+    *candidates = 2;
+    for (uint64_t pos = 1; pos < (N+1)/2 ; pos++){
+        *(candidates + pos) = (pos-1)*2 + 3;
     }
-    for (long pos = 2; pos < (N/2) +1 ; pos++){
-        if (result[pos] > 0){
-            for (long spos = pos+result[pos]; spos < (N/2)+1; spos++){
-                result[spos] = 0;
+    for (uint64_t pos = 2; pos < (N+1)/2 ; pos++){
+        if (*(candidates + pos) > 0){
+            uint64_t candidate = *(candidates + pos);
+            for (uint64_t spos = pos + candidate; spos < (N/2)+1; spos += candidate){
+                *(candidates + spos) = 0;
             }
         }
     }
-    return result;
+    uint32_t cont = 0;
+    for (uint64_t pos = 0; pos < (N+1)/2; pos++) if (*(candidates + pos)) cont++;
+    printf("Allocating memory for result: %u bytes\n", cont * sizeof(uint64_t));
+    uint64_t *sieve = (uint64_t *) calloc(cont + 1, sizeof(uint64_t));
+    cont = 0;
+    for (uint64_t pos = 0; pos < (N+1)/2; pos++) {
+        if(*(candidates + pos)){
+            *(sieve + cont) = *(candidates + pos);
+            cont++;
+        }
+    }
+    free(candidates);
+    return sieve;
+}
 
+uint32_t* sieve_u32( uint32_t N){
+    printf("Allocating memory for candidates: %u bytes\n", ((N+1)/2 * sizeof(uint32_t)));
+    uint32_t *candidates = (uint32_t *) calloc(((N+1)/2), sizeof(uint32_t));
+    *candidates = 2;
+    for (uint32_t pos = 1; pos < (N+1)/2 ; pos++){
+        *(candidates + pos) = (pos-1)*2 + 3;
+    }
+    for (uint32_t pos = 2; pos < (N+1)/2 ; pos++){
+        if (*(candidates + pos) > 0){
+            uint32_t candidate = *(candidates + pos);
+            for (uint32_t spos = pos + candidate; spos < (N/2)+1; spos += candidate){
+                *(candidates + spos) = 0;
+            }
+        }
+    }
+    uint32_t cont = 0;
+    for (uint32_t pos = 0; pos < (N+1)/2; pos++) if (*(candidates + pos)) cont++;
+    printf("Allocating memory for result: %u bytes\n", cont * sizeof(uint32_t));
+    uint32_t *sieve = (uint32_t *) calloc(cont + 1, sizeof(uint32_t));
+    cont = 0;
+    for (uint32_t pos = 0; pos < (N+1)/2; pos++) {
+        if(*(candidates + pos)){
+            *(sieve + cont) = *(candidates + pos);
+            cont++;
+        }
+    }
+    free(candidates);
+    return sieve;
+}
+
+uint32_t* factorize(uint32_t n, uint32_t *primes){
+    uint32_t MaxPrime = (uint32_t) (sqrt(n)+1);
+    if (primes == NULL){
+        primes = sieve_u32(MaxPrime);
+    }
+    uint32_t *factors = (uint32_t *) calloc(MaxPrime, sizeof(uint32_t));
+    uint32_t acc = n;
+    uint32_t cont = 0;
+    while(*primes){
+        if (acc % ((uint64_t) (*primes)) == 0){
+            *(factors + cont) = *primes;
+            acc /= *primes;
+            cont++;
+        }
+        else{
+            primes++;
+        }
+    }
+    factors = (uint32_t *) realloc(factors, (cont+1) * sizeof(uint32_t));
+    return factors;
+}
+
+bool isprime(uint64_t n, uint64_t *primes){
+    while(*primes){
+        if (n % ((uint64_t) (*primes)) == 0) return false;
+        primes++;
+    }
+    return true;
 }
 
 
+int main(void){
+    uint32_t Limit = 1e8;
+    uint32_t MaxPrime = ((uint32_t) sqrt(Limit)) + 1;
+    uint32_t *primes = sieve_u32(MaxPrime);
+    uint32_t cont = 0;
+    for(uint32_t n = 2; n < Limit; n++) {
+        uint32_t *factors = factorize(n, primes);
+        uint32_t MaxFactor = 0;
+        while (*factors){
+            if (*factors > MaxFactor) MaxFactor = *factors;
+            factors++;
+        }
+        if (MaxFactor == 5) cont++;
+    }
+    printf("Total: %u", cont);
+    free(primes);
+    return 0;
+}
