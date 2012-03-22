668cec1f3e86329b9e95289f0335541e4f8978b5
icot <icot@users.noreply.github.com>
2012-03-22 19:34:59 +0100

    Fixed G2J, doubles

--- p84.py
+++ p84.py
@@ -13,7 +13,35 @@ def chance(pos):
         return pos 
 
 def community_chest(pos):
-    global chest
+    
+    def next_utility(pos):
+        if pos < 12 or pos >= 28: 
+            return 12
+        else:
+            return 28
+
+    def next_railway(pos):
+        if 5 <= pos < 15:
+            return 15
+        elif 15 <= pos < 25:
+            return 25
+        elif 25 <= pos < 35:
+            return 35
+        else:
+            return 5
+    
+    chest = {n:None for n in range(1,17)}
+    chest[1] = 0
+    chest[2] = 10
+    chest[3] = 11
+    chest[4] = 34
+    chest[5] = 39
+    chest[6] = 5
+    chest[7] = next_railway
+    chest[8] = next_railway
+    chest[9] = next_utility
+    chest[10] = lambda x: x-3 
+    
     c = random.randint(1,16)
     if type(chest[c]) == type(lambda x:x):
         return chest[c](pos)
@@ -22,24 +50,8 @@ def community_chest(pos):
     else:
         return pos
 
-def next_utility(pos):
-    if pos < 12 or pos > 28: 
-        return 12
-    else:
-        return 28
-
-def next_railway(pos):
-    if 5 < pos < 15:
-        return 15
-    elif 15 < pos < 25:
-        return 25
-    elif 25 < pos < 35:
-        return 35
-    else:
-        return 5
-
 def rolls():
-    return random.randint(1,6) + random.randint(1,6)
+    return (random.randint(1,6), random.randint(1,6))
 
 def step(pos, d):
     global board
@@ -50,10 +62,8 @@ def step(pos, d):
     elif type(action) == type(lambda x:x):
         res = action(nposition)
         if res < 0:
-            res =  39 - nposition + 1
+            res =  39 + res + 1
         return res
-    else:
-        return action
 
 board = {n:None for n in range(40)}
 board[2] = community_chest
@@ -62,27 +72,24 @@ board[33] = community_chest
 board[7] = chance
 board[22] = chance
 board[36] = chance
-board[30] = 10
-
-chest = {n:None for n in range(1,17)}
-chest[1] = 0
-chest[2] = 10
-chest[3] = 11
-chest[4] = 34
-chest[5] = 39
-chest[6] = 5
-chest[7] = next_railway
-chest[8] = next_railway
-chest[9] = next_utility
-chest[10] = lambda x: x-3 
+board[30] = lambda x: 10
 
 def main():
-    N = 1000000
+    N = 10000000
     pos = 0
     visited = {n:0 for n in range(40)}
+    doubles = 0
     for n in range(N):
         dices = rolls() 
-        pos = step(pos, dices)
+        if dices[0] == dices[1]:
+            doubles += 1
+        if doubles == 3:
+            pos = 10
+            doubles = 0
+        else:
+            pos = step(pos, sum(dices))
+        if pos == 30:
+            pos = 10
         visited[pos] += 1
 
     data = {v:k for k,v in visited.items()}
