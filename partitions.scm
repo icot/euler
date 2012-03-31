ffd4c2231b89bc63083aed61660b2c436e123eb8
icot <icot@users.noreply.github.com>
2012-03-31 20:40:01 +0200

    Added partitions.scm

--- partitions.scm
+++ partitions.scm
@@ -1,18 +1,18 @@
-#lang racket
+
+(require-extension srfi-1)
 
 (define (neg_unit_pow k)
-  (let ([r (modulo k 2)])
+  (let ((r (modulo k 2)))
     (if (= r 1) -1 1)))
-
-(define (range a b)
-  (for/list ([i (stop-after (in-naturals a) (lambda (x) (>= x b)))]) i))
 
 (define (partitions n cache)
   (cond ((<= n 1) 1)
         ((= n 2) 2)
-        (else (let* ([ks (range 1 n)]
+        (else (let* ([ks (iota (sub1 n) 1 1)]
                      [j1 (filter (lambda (x) (< x n)) (map (lambda (k) (/ (- (* 3 k k) k) 2)) ks))]
-                     [j2 (filter (lambda (x) (< x n)) (map (lambda (k) (/ (+ (* 3 k k) k) 2)) ks))])
-                j2))))
+                     [j2 (filter (lambda (x) (< x n)) (map (lambda (k) (/ (+ (* 3 k k) k) 2)) ks))]
+                     [kj1 (zip j1 ks)]
+                     [kj2 (zip j2 ks)])
+                (cons kj1 kj2) ))))
 
-(partitions 10 '())
+(printf "Result: ~A" (partitions 10 '()))
