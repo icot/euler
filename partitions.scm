46b0eab97424d9d23cb1f827aa571a7e971d9fb2
icot <icot@users.noreply.github.com>
2012-04-01 16:41:16 +0200

    Added partitions.rkt

--- partitions.scm
+++ partitions.scm
@@ -1,18 +1,50 @@
+; Computes number of partitions using the Macmahon formula
 
 (require-extension srfi-1)
+(require-extension srfi-69)
 
 (define (neg_unit_pow k)
   (let ((r (modulo k 2)))
     (if (= r 1) -1 1)))
 
+(define cached_values (make-hash-table))
+
 (define (partitions n cache)
   (cond ((<= n 1) 1)
         ((= n 2) 2)
+        ((hash-table-exists? cache n) (hash-table-ref cache n))
         (else (let* ([ks (iota (sub1 n) 1 1)]
-                     [j1 (filter (lambda (x) (< x n)) (map (lambda (k) (/ (- (* 3 k k) k) 2)) ks))]
-                     [j2 (filter (lambda (x) (< x n)) (map (lambda (k) (/ (+ (* 3 k k) k) 2)) ks))]
+                     [j1 (filter (lambda (x) (<= x n)) 
+                                 (map (lambda (k) (/ (- (* 3 k k) k) 2)) ks))]
+                     [j2 (filter (lambda (x) (<= x n)) 
+                                 (map (lambda (k) (/ (+ (* 3 k k) k) 2)) ks))]
                      [kj1 (zip j1 ks)]
-                     [kj2 (zip j2 ks)])
-                (cons kj1 kj2) ))))
+                     [kj2 (zip j2 ks)]
+                     [pkj1 (map (lambda (p) (* (neg_unit_pow (sub1 (last p))) 
+                                               (partitions (- n (first p)) cache))) kj1)]
+                     [pkj2 (map (lambda (p) (* (neg_unit_pow (sub1 (last p))) 
+                                               (partitions (- n (first p)) cache))) kj2)]
+                     [pn (append pkj1 pkj2)])
+                (begin 
+                  ;(printf " N: ~A kj1: ~A kj2: ~A\n" n kj1 kj2)
+                  (hash-table-set! cache n (fold + 0 pn))
+                  (hash-table-ref cache n)) ))))
 
-(printf "Result: ~A" (partitions 10 '()))
+(define (parts n cached_values)
+  (let ([pn (partitions n cached_values)])
+        (begin
+          (printf "n: ~A, p(n): ~A\n" n pn)
+          pn)))
+
+; (for-each (lambda (x) (parts x cached_values)) (iota 11))
+
+(define (main n)
+  (let* ([pn (partitions n cached_values)]
+         [mod (remainder pn 1000000)])
+    (begin
+      (printf "n: ~A mod: ~A p(n): ~A\n" n mod pn)
+      (if (= mod 0)
+        #t
+        (main (add1 n))))))
+
+(main 1)
