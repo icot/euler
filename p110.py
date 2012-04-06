0be8e4cf8e3def2d7adb78c20a23e60f8e306b50
icot <icot@users.noreply.github.com>
2012-04-06 19:04:10 +0200

    Solved p110

--- p110.py
+++ p110.py
@@ -10,7 +10,6 @@ def main():
     nsols = int(4000000)
     limit = 2 * nsols + 1
     K = ceil(log(2*nsols)/log(3))
-    K = 100
     print K
     sols = {}
     for k in range(1,int(K)+2):
@@ -22,14 +21,14 @@ def main():
             candidates[ndivs] = (contrib, p)
         keys = candidates.keys()
         keys.sort()
-        keys = [key for key in keys if key >= nsols]
+        keys = [key for key in keys if key >= limit]
         if keys:
             key = keys[0]
             contrib, a = candidates[key]
             a.reverse()
             prods = [pow(pi, ai) for pi, ai in zip(primes, a)]
             num = reduce(lambda x, y: x * y, prods)
-            print k, key, prods[:7], prods[-1], num
+            print num, k, key, prods[:7], prods[-1]
             try:
                 if num < sols[key]:
                     sols[key] = num
