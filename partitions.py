f00abb59355ef7a6d82aea20f5066f678611419d
icot <icot@users.noreply.github.com>
2012-04-06 18:43:43 +0200

    Added partition_gen to partitions.py

--- partitions.py
+++ partitions.py
@@ -6,14 +6,14 @@ from math import exp, sqrt, floor, ceil, log
 from utils import memoize
 from itertools import product, chain
 
-def partitions(n):
+def partitions_gen(n):
     # base case of recursion: zero is the sum of the empty list
     if n == 0:
         yield []
         return
         
     # modify partitions of n-1 to form partitions of n
-    for p in partitions(n-1):
+    for p in partitions_gen(n-1):
         yield [1] + p
         if p and (len(p) < 2 or p[1] > p[0]):
             yield [p[0] + 1] + p[1:]
